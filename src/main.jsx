98418d0a05bc1b04f3f91d3d9fd43699812cc2b5
Petros Belachew <petrosbelachew@gmail.com>
2025-11-15 09:17:15 +0300

    fix: test

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import SearchInput from "./common/searchinput.jsx";
 import CurrentWeatherDisplay from "./home/current-weather-display.jsx";
 import ForecastDisplay from "./home/forecast-display.jsx";
 
